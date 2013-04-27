a8e79c4ef92b9d3960ebff0956e3ed3d5f6bc313
Jochen Eddelbüttel <jochen@eddelbuettel.net>
2013-04-27 14:07:43 +0300

    Update FilteredNodeChildren.java
    jira-6125

--- subprojects/groovy-xml/src/main/java/groovy/util/slurpersupport/FilteredNodeChildren.java
+++ subprojects/groovy-xml/src/main/java/groovy/util/slurpersupport/FilteredNodeChildren.java
@@ -1,18 +1,18 @@
 /*
- * Copyright 2003-2010 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2003-2010 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package groovy.util.slurpersupport;
 
 import java.util.Iterator;
@@ -23,49 +23,23 @@ import org.codehaus.groovy.runtime.typehandling.DefaultTypeTransformation;
 import groovy.lang.Closure;
 
 /**
- * Lazy evaluated representation of child nodes filtered by a Closure.
- *
- * @author John Wilson
- */
+* Lazy evaluated representation of child nodes filtered by a Closure.
+*
+* @author John Wilson, Jochen Eddel+
+*/
 public class FilteredNodeChildren extends NodeChildren {
     private final Closure closure;
 
     /**
-     * @param parent the GPathResult prior to the application of the expression creating this GPathResult
-     * @param closure the Closure to use to filter the nodes
-     * @param namespaceTagHints the known tag to namespace mappings
-     */
+* @param parent the GPathResult prior to the application of the expression creating this GPathResult
+* @param closure the Closure to use to filter the nodes
+* @param namespaceTagHints the known tag to namespace mappings
+*/
     public FilteredNodeChildren(final GPathResult parent, final Closure closure, final Map<String, String> namespaceTagHints) {
         super(parent, parent.name, namespaceTagHints);
         this.closure = closure;
     }
-
-    public Iterator iterator() {
-        return new Iterator() {
-            final Iterator iter = FilteredNodeChildren.this.parent.iterator();
-            Object next = null;
-
-            public boolean hasNext() {
-                while (this.iter.hasNext()) {
-                    final Object childNode = this.iter.next();
-                    if (closureYieldsTrueForNode(childNode)) {
-                        this.next = childNode;
-                        return true;
-                    }
-                }
-                return false;
-            }
-
-            public Object next() {
-                return this.next;
-            }
-
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-        };
-    }
-
+    
     public Iterator nodeIterator() {
         return new NodeIterator(this.parent.nodeIterator()) {
             protected Object getNextNode(final Iterator iter) {
