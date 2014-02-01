d6056a53e37105f739bd28d761a22ecf26d5ac01
Johannes Link <business@johanneslink.net>
2014-02-01 18:07:45 +0100

    Added RecursiveListExamples which do not work yet

--- src/test/org/codehaus/groovy/transform/tailrec/RecursiveListExamples.groovy
+++ src/test/org/codehaus/groovy/transform/tailrec/RecursiveListExamples.groovy
@@ -1,4 +1,65 @@
 package org.codehaus.groovy.transform.tailrec
 
+import groovy.transform.CompileStatic
+import groovy.transform.TailRecursive
+import org.junit.Test
+
+/**
+ * @author Johannes Link
+ */
 class RecursiveListExamples {
+
+    def rlist
+
+    @Test
+    void creating() {
+        rlist = RecursiveList.rlist([1, 2, 3, 4])
+        assert RecursiveList.toList(rlist) == [1, 2, 3, 4]
+    }
+
+    @Test
+    void counting() {
+        List integers = new ArrayList(1..9999)
+        rlist = RecursiveList.rlist(integers)
+        assert RecursiveList.count(rlist) == 9999
+    }
+}
+
+@CompileStatic
+class RecursiveList {
+    static final Map empty = [:]
+
+    static Map cons(Object element, Map list) {
+        [head: element, tail: list]
+    }
+
+    static Map tail(Map rlist) {
+        (Map) rlist.tail
+    }
+
+    static Object head(Map rlist) {
+        rlist.head
+    }
+
+    @TailRecursive
+    static Map rlist(List elements, Map result = empty) {
+        if (!elements)
+            return result
+        Object head = elements.pop()
+        rlist(elements, cons(head, result))
+    }
+
+    @TailRecursive
+    static List toList(Map rlist, List result = []) {
+        if (!rlist)
+            return result
+        toList(tail(rlist), result << head(rlist) as List)
+    }
+
+    @TailRecursive
+    static int count(Map rlist, int result = 0) {
+        if (!rlist)
+            return result
+        count(tail(rlist), ++result)
+    }
 }
