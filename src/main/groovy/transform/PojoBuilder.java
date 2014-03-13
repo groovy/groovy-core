6227128e6eaa4bf468f2f1628147fa335b7caec9
Marcin Zajaczkowski <szpak@users.sourceforge.net>
2014-03-14 00:58:26 +0100

    #341 - minor tweaks in PojoBuilder

--- src/main/groovy/transform/PojoBuilder.java
+++ src/main/groovy/transform/PojoBuilder.java
@@ -4,10 +4,11 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.annotation.Documented;
 
 import org.codehaus.groovy.transform.GroovyASTTransformationClass;
 
-@java.lang.annotation.Documented
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE})
 @GroovyASTTransformationClass("org.codehaus.groovy.transform.PojoBuilderASTTransformation")
