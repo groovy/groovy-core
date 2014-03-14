6c6c5fe01e26221b2421c66431a30cd4effab98c
Marcin Grzejszczak <marcin@grzejszczak.pl>
2014-03-14 12:56:50 +0100

    PojoBuilder changes following code review - added missing license, JavaDocs and converted tabs to spaces

--- src/main/org/codehaus/groovy/transform/PojoBuilderASTTransformation.java
+++ src/main/org/codehaus/groovy/transform/PojoBuilderASTTransformation.java
@@ -60,143 +60,143 @@ public class PojoBuilderASTTransformation extends AbstractASTTransformation {
     public void visit(ASTNode[] nodes, SourceUnit source) {
         init(nodes, source);
         AnnotatedNode parent = (AnnotatedNode) nodes[1];
-        AnnotationNode anno = (AnnotationNode) nodes[0];
-        if (!MY_TYPE.equals(anno.getClassNode())) return;
+        AnnotationNode annotation = (AnnotationNode) nodes[0];
+        if (!MY_TYPE.equals(annotation.getClassNode())) return;
 
         if (parent instanceof ClassNode) {
             ClassNode annotatedClassNode = (ClassNode) parent;
             checkNotInterface(annotatedClassNode, MY_TYPE_NAME);
-	        ClassNode classToCreateBuilderFor = getClassToCreateBuilderFor(anno);
-	        if (classToCreateBuilderFor == null) {
-		        addError("Error during " + MY_TYPE_NAME + " processing: You have to provide the class for which you want to create the builder", anno);
-		        return;
-	        }
-	        List<String> excludes = getMemberList(anno, "excludes");
-	        List<String> includes = getMemberList(anno, "includes");
-	        if (hasAnnotation(annotatedClassNode, CanonicalASTTransformation.MY_TYPE)) {
-		        AnnotationNode canonical = annotatedClassNode.getAnnotations(CanonicalASTTransformation.MY_TYPE).get(0);
-		        if (excludes == null || excludes.isEmpty()) excludes = getMemberList(canonical, "excludes");
-		        if (includes == null || includes.isEmpty()) includes = getMemberList(canonical, "includes");
-	        }
-	        if (includes != null && !includes.isEmpty() && excludes != null && !excludes.isEmpty()) {
-		        addError("Error during " + MY_TYPE_NAME + " processing: Only one of 'includes' and 'excludes' should be supplied not both.", anno);
-		        return;
-	        }	        
-	        List<FieldNode> classToCreateBuilderForFields = getAllFieldsOfClassWithIncludesExcludes(classToCreateBuilderFor, includes, excludes);
-	        List<FieldNode> createdPrivateFields = createPrivateFieldsForClass(annotatedClassNode, classToCreateBuilderForFields);
-	        appendWithBuilderMethodsForPrivateFields(annotatedClassNode, createdPrivateFields);
-	        appendBuildMethod(annotatedClassNode, classToCreateBuilderFor, createdPrivateFields);
-	        appendBuildMethodWithValidationClosure(annotatedClassNode, classToCreateBuilderFor, createdPrivateFields);
+            ClassNode pojoClassNode = getPojoClassNodeToCreateBuilderFor(annotation);
+            if (pojoClassNode == null) {
+                addError("Error during " + MY_TYPE_NAME + " processing: You have to provide the class for which you want to create the builder", annotation);
+                return;
+            }
+            List<String> excludes = getMemberList(annotation, "excludes");
+            List<String> includes = getMemberList(annotation, "includes");
+            if (hasAnnotation(annotatedClassNode, CanonicalASTTransformation.MY_TYPE)) {
+                AnnotationNode canonical = annotatedClassNode.getAnnotations(CanonicalASTTransformation.MY_TYPE).get(0);
+                if (excludes == null || excludes.isEmpty()) excludes = getMemberList(canonical, "excludes");
+                if (includes == null || includes.isEmpty()) includes = getMemberList(canonical, "includes");
+            }
+            if (includes != null && !includes.isEmpty() && excludes != null && !excludes.isEmpty()) {
+                addError("Error during " + MY_TYPE_NAME + " processing: Only one of 'includes' and 'excludes' should be supplied not both.", annotation);
+                return;
+            }
+            List<FieldNode> classToCreateBuilderForFields = getAllFieldsOfClassWithIncludesExcludes(pojoClassNode, includes, excludes);
+            List<FieldNode> createdPrivateFields = createPrivateFieldsForClass(annotatedClassNode, classToCreateBuilderForFields);
+            appendWithBuilderMethodsForPrivateFields(annotatedClassNode, createdPrivateFields);
+            appendBuildMethod(annotatedClassNode, pojoClassNode, createdPrivateFields);
+            appendBuildMethodWithValidationClosure(annotatedClassNode, pojoClassNode, createdPrivateFields);
         }
     }
 
-	private static ClassNode getClassToCreateBuilderFor(AnnotationNode anno) {
-		final Expression forClass = anno.getMember("forClass");
-		if (forClass != null) {
-			return forClass.getType();
-		}
-		return null;
-	}
+    private static ClassNode getPojoClassNodeToCreateBuilderFor(AnnotationNode anno) {
+        final Expression forClass = anno.getMember("forClass");
+        if (forClass != null) {
+            return forClass.getType();
+        }
+        return null;
+    }
 
-	private static List<FieldNode> createPrivateFieldsForClass(ClassNode cNode, List<FieldNode> classToCreateBuilderForFields) {
-		List<FieldNode> createdPrivateFields = new ArrayList<FieldNode>();
-		for (FieldNode field : classToCreateBuilderForFields) {
-			createdPrivateFields.add(field);
-			cNode.addField(field);
-		}
-		return createdPrivateFields;
-	}
+    private static List<FieldNode> createPrivateFieldsForClass(ClassNode cNode, List<FieldNode> classToCreateBuilderForFields) {
+        List<FieldNode> createdPrivateFields = new ArrayList<FieldNode>();
+        for (FieldNode field : classToCreateBuilderForFields) {
+            createdPrivateFields.add(field);
+            cNode.addField(field);
+        }
+        return createdPrivateFields;
+    }
 
-	private static List<FieldNode> getAllFieldsOfClassWithIncludesExcludes(ClassNode classToCreateBuilderFor, List<String> includes, List<String> excludes) {
-		List<FieldNode> declaredFields = classToCreateBuilderFor.getFields();
-		List<FieldNode> fieldsToInclude = new ArrayList<FieldNode>();
-		if (!includes.isEmpty()) {
-			for (FieldNode field : declaredFields) {
-				if (includes.contains(field.getName())) {
-					fieldsToInclude.add(field);
-				}
-			}
-		} else if (!excludes.isEmpty()) {
-			for (FieldNode field : declaredFields) {
-				if (!excludes.contains(field.getName())) {
-					fieldsToInclude.add(field);
-				}
-			}
-		} else {
-			fieldsToInclude = declaredFields;
-		}
-		return fieldsToInclude;
-	}
+    private static List<FieldNode> getAllFieldsOfClassWithIncludesExcludes(ClassNode classToCreateBuilderFor, List<String> includes, List<String> excludes) {
+        List<FieldNode> declaredFields = classToCreateBuilderFor.getFields();
+        List<FieldNode> fieldsToInclude = new ArrayList<FieldNode>();
+        if (!includes.isEmpty()) {
+            for (FieldNode field : declaredFields) {
+                if (includes.contains(field.getName())) {
+                    fieldsToInclude.add(field);
+                }
+            }
+        } else if (!excludes.isEmpty()) {
+            for (FieldNode field : declaredFields) {
+                if (!excludes.contains(field.getName())) {
+                    fieldsToInclude.add(field);
+                }
+            }
+        } else {
+            fieldsToInclude = declaredFields;
+        }
+        return fieldsToInclude;
+    }
 
-	private static void appendWithBuilderMethodsForPrivateFields(ClassNode annotatedClassNode, List<FieldNode> createdPrivateFields) {
-		for (FieldNode fieldNode : createdPrivateFields) {
-			MethodNode withBuilderMethodForField = createWithBuilderMethodForField(annotatedClassNode, fieldNode);
-			annotatedClassNode.addMethod(withBuilderMethodForField);
-		} 
-	}
+    private static void appendWithBuilderMethodsForPrivateFields(ClassNode annotatedClassNode, List<FieldNode> createdPrivateFields) {
+        for (FieldNode fieldNode : createdPrivateFields) {
+            MethodNode withBuilderMethodForField = createWithBuilderMethodForField(annotatedClassNode, fieldNode);
+            annotatedClassNode.addMethod(withBuilderMethodForField);
+        } 
+    }
 
-	private static MethodNode createWithBuilderMethodForField(ClassNode annotatedClassNode, FieldNode fieldNode) {
-		final BlockStatement body = new BlockStatement();
+    private static MethodNode createWithBuilderMethodForField(ClassNode annotatedClassNode, FieldNode fieldNode) {
+        final BlockStatement body = new BlockStatement();
         String fieldName = fieldNode.getName();
         Parameter parameter = new Parameter(fieldNode.getType(), fieldName);
         // this.field = parameter
         body.addStatement(assignStatement(new FieldExpression(fieldNode), new VariableExpression(parameter)));
-		// return this
-		body.addStatement(new ReturnStatement(VariableExpression.THIS_EXPRESSION));
+        // return this
+        body.addStatement(new ReturnStatement(VariableExpression.THIS_EXPRESSION));
         String firstLetterUppercasedFieldName = Character.toUpperCase(fieldName.charAt(0)) + fieldName.substring(1);
-		// Builder withField(type parameter) { this.field = parameter; return this; }
+        // Builder withField(type parameter) { this.field = parameter; return this; }
         return new MethodNode("with" + firstLetterUppercasedFieldName, ACC_PUBLIC, annotatedClassNode.getPlainNodeReference(),
                 new Parameter[] {parameter}, ClassNode.EMPTY_ARRAY, body);
-	}
+    }
 
-	private static void appendBuildMethod(ClassNode annotatedClassNode, ClassNode classToCreateBuilderFor, List<FieldNode> createdPrivateFields) {
-		MethodNode buildMethodForField = createBuildMethodForField(classToCreateBuilderFor, createdPrivateFields);
-		annotatedClassNode.addMethod(buildMethodForField);
-	}
+    private static void appendBuildMethod(ClassNode annotatedClassNode, ClassNode classToCreateBuilderFor, List<FieldNode> createdPrivateFields) {
+        MethodNode buildMethodForField = createBuildMethodForField(classToCreateBuilderFor, createdPrivateFields);
+        annotatedClassNode.addMethod(buildMethodForField);
+    }
 
-	private static MethodNode createBuildMethodForField(ClassNode classToCreateBuilderFor, List<FieldNode> createdPrivateFields) {
-		final BlockStatement body = new BlockStatement();
-		VariableExpression classToCreateBuilderForVariable = initializeObjectAndSetItsValues(classToCreateBuilderFor, createdPrivateFields, body);
-		// return objectToBuild
-		body.addStatement(new ReturnStatement(classToCreateBuilderForVariable));
-		// ObjectToBuild build() { ObjectToBuild objectToBuild = new ObjectToBuild(); SET_VALUES; return objectToBuild;}
-		return new MethodNode("build", ACC_PUBLIC, classToCreateBuilderFor, Parameter.EMPTY_ARRAY, ClassNode.EMPTY_ARRAY, body);
-	}
+    private static MethodNode createBuildMethodForField(ClassNode classToCreateBuilderFor, List<FieldNode> createdPrivateFields) {
+        final BlockStatement body = new BlockStatement();
+        VariableExpression classToCreateBuilderForVariable = initializeObjectAndSetItsValues(classToCreateBuilderFor, createdPrivateFields, body);
+        // return objectToBuild
+        body.addStatement(new ReturnStatement(classToCreateBuilderForVariable));
+        // ObjectToBuild build() { ObjectToBuild objectToBuild = new ObjectToBuild(); SET_VALUES; return objectToBuild;}
+        return new MethodNode("build", ACC_PUBLIC, classToCreateBuilderFor, Parameter.EMPTY_ARRAY, ClassNode.EMPTY_ARRAY, body);
+    }
 
-	private static VariableExpression initializeObjectAndSetItsValues(ClassNode classToCreateBuilderFor, List<FieldNode> createdPrivateFields,
-	                                                                  BlockStatement body) {
-		final Expression callClassToCreateBuilderForConstructor = new ConstructorCallExpression(classToCreateBuilderFor, ArgumentListExpression.EMPTY_ARGUMENTS);
-		VariableExpression classToCreateBuilderForVariable = new VariableExpression("objectToBuild", classToCreateBuilderFor);
-		// ObjectToBuild objectToBuild = new ObjectToBuild();
+    private static VariableExpression initializeObjectAndSetItsValues(ClassNode classToCreateBuilderFor, List<FieldNode> createdPrivateFields,
+                                                                      BlockStatement body) {
+        final Expression callClassToCreateBuilderForConstructor = new ConstructorCallExpression(classToCreateBuilderFor, ArgumentListExpression.EMPTY_ARGUMENTS);
+        VariableExpression classToCreateBuilderForVariable = new VariableExpression("objectToBuild", classToCreateBuilderFor);
+        // ObjectToBuild objectToBuild = new ObjectToBuild();
         Statement constructorCallToVariableAssignment = declStatement(classToCreateBuilderForVariable, callClassToCreateBuilderForConstructor);
-		body.addStatement(constructorCallToVariableAssignment);
-		// objectToBuild.property = field; 
-		for (FieldNode createdPrivateField : createdPrivateFields) {
-			PropertyExpression propertyExpressionForClass = new PropertyExpression(classToCreateBuilderForVariable, createdPrivateField.getName());
-			Statement setPropertyOnObject = assignStatement(propertyExpressionForClass, new VariableExpression(createdPrivateField));
-			body.addStatement(setPropertyOnObject);
-		}
-		return classToCreateBuilderForVariable;
-	}
+        body.addStatement(constructorCallToVariableAssignment);
+        // objectToBuild.property = field; 
+        for (FieldNode createdPrivateField : createdPrivateFields) {
+            PropertyExpression propertyExpressionForClass = new PropertyExpression(classToCreateBuilderForVariable, createdPrivateField.getName());
+            Statement setPropertyOnObject = assignStatement(propertyExpressionForClass, new VariableExpression(createdPrivateField));
+            body.addStatement(setPropertyOnObject);
+        }
+        return classToCreateBuilderForVariable;
+    }
 
-	private void appendBuildMethodWithValidationClosure(ClassNode annotatedClassNode, ClassNode classToCreateBuilderFor, List<FieldNode> createdPrivateFields) {		
-		MethodNode methodNode = createBuildMethodWithValidationClosure(classToCreateBuilderFor, createdPrivateFields);
-		annotatedClassNode.addMethod(methodNode);
-	}
+    private void appendBuildMethodWithValidationClosure(ClassNode annotatedClassNode, ClassNode classToCreateBuilderFor, List<FieldNode> createdPrivateFields) {        
+        MethodNode methodNode = createBuildMethodWithValidationClosure(classToCreateBuilderFor, createdPrivateFields);
+        annotatedClassNode.addMethod(methodNode);
+    }
 
-	private MethodNode createBuildMethodWithValidationClosure(ClassNode classToCreateBuilderFor, List<FieldNode> createdPrivateFields) {
-		final BlockStatement body = new BlockStatement();
-		// ObjectToBuild objectToBuild = new ObjectToBuild();
-		// set values
-		VariableExpression classToCreateBuilderForVariable = initializeObjectAndSetItsValues(classToCreateBuilderFor, createdPrivateFields, body);
+    private MethodNode createBuildMethodWithValidationClosure(ClassNode classToCreateBuilderFor, List<FieldNode> createdPrivateFields) {
+        final BlockStatement body = new BlockStatement();
+        // ObjectToBuild objectToBuild = new ObjectToBuild();
+        // set values
+        VariableExpression classToCreateBuilderForVariable = initializeObjectAndSetItsValues(classToCreateBuilderFor, createdPrivateFields, body);
         Parameter parameter = new Parameter(ClassHelper.CLOSURE_TYPE.getPlainNodeReference(), "validationClosure");
-		// call validation closure with created object as parameter
+        // call validation closure with created object as parameter
         MethodCallExpression closureCall = new MethodCallExpression(new VariableExpression(parameter), "call", classToCreateBuilderForVariable);
-		body.addStatement(new ExpressionStatement(closureCall));
-		// return objectToBuild
-		body.addStatement(new ReturnStatement(classToCreateBuilderForVariable));
-		// ObjectToBuild build(Closure validationClosure) { ObjectToBuild objectToBuild = new ObjectToBuild(); SET_VALUES; validationClosure.call(objectToBuild); return objectToBuild;}
+        body.addStatement(new ExpressionStatement(closureCall));
+        // return objectToBuild
+        body.addStatement(new ReturnStatement(classToCreateBuilderForVariable));
+        // ObjectToBuild build(Closure validationClosure) { ObjectToBuild objectToBuild = new ObjectToBuild(); SET_VALUES; validationClosure.call(objectToBuild); return objectToBuild;}
         return new MethodNode("build", ACC_PUBLIC, classToCreateBuilderFor, new Parameter[] {parameter}, ClassNode.EMPTY_ARRAY, body);
-	}
+    }
 
 }
